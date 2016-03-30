950cd520ea4cd583709074dc6b0f6c6d4732f3e7
mezaeduardo011 <mezaeduardo011@gmail.com>
2016-03-30 13:02:00 -0430

    Arreglos

--- protected/models/actividadModel.php
+++ protected/models/actividadModel.php
@@ -23,10 +23,7 @@
 		}
 
 		public function getActividadUnica($id){
-				$query = "SELECT ROW_NUMBER() OVER (ORDER BY A.id_actividad_institucional) AS numeracion,*,R.referencia as status FROM actividad_institucional A
-						INNER JOIN persona_x_actividad_institucional B ON A.id_actividad_institucional = B.id_actividad_institucional
-						INNER JOIN referencial R ON R.id_referencial = A.status
-						WHERE A.id_actividad_institucional='$id'";			
+				$query = "SELECT A.* FROM actividad_institucional A WHERE a.id_actividad_institucional=$id";			
 						$result=$this->selectPdo($query);
 						return $result;
 					
@@ -35,19 +32,24 @@
 		public function getDetalles($id = FALSE){
 
 						$query = "SELECT ROW_NUMBER() OVER (ORDER BY P.id_persona) AS numeracion,
-								P.nombre,P.nombre2,P.apellido,P.apellido2,P.telefono , B.nombre_actividad, B.ubicacion
+								P.nombre,P.nombre2,P.apellido,P.apellido2,P.telefono , B.nombre_actividad, B.ubicacion,ref.referencia as coordinacion
 								from persona_x_actividad_institucional A
 								INNER JOIN persona P on P.id_persona=A.id_persona
 								INNER JOIN actividad_institucional B on B.id_actividad_institucional = A.id_actividad_institucional
+								INNER JOIN persona_empleada as per ON per.id_persona=p.id_persona
+								INNER JOIN referencial as Ref on ref.id_referencial=per.coordinacion_referencial
 								where A.id_actividad_institucional = '$id'";
 						$result=$this->selectPdo($query);
 						return $result;
 					
 				}				
-		public function finalizarActividad($id = FALSE){
-						$query = "UPDATE actividad_institucional SET status=97 WHERE id_actividad_institucional = '$id'";
-						$result=$this->selectPdo($query);
-						return $result;					
+		public function finalizarActividad($id = FALSE,$finalizar=FALSE){			
+						if($finalizar!=FALSE){
+						    return $result=$this->selectPdo($query = "UPDATE actividad_institucional SET status=97 WHERE id_actividad_institucional = '$id'");
+						}else{
+							return $result=$this->selectPdo($query = "UPDATE actividad_institucional SET status=98 WHERE id_actividad_institucional = '$id'");
+						}				
+						
 				}
 }
 ?>
--- public/js/actividades/consulta.js
+++ public/js/actividades/consulta.js
@@ -1,7 +1,7 @@
 $(document).ready(function () {
             var BASE_URL = "http://localhost/catic/"; 
-            var grid_selector = "#jqGrid";
-            var grid_pager = "#jqGridPager";
+            var grid_selector = "#actividades";
+            var grid_pager = "#pagerActividades";
 
             //resize to fit page size
             $(window).on('resize.jqGrid', function () {
@@ -13,7 +13,7 @@ $(document).ready(function () {
             "viewrecords":true,
             "jsonReader":{"repeatitems":false,"subgrid":{"repeatitems":false}},
             "gridview":true,
-             url: BASE_URL+"vacaciones/personal_vacaciones",
+             url: BASE_URL+"actividad_institucional/getActividades",
             "loadonce": true,
             "rowNum":10,
             "height":200,
@@ -23,12 +23,17 @@ $(document).ready(function () {
             "datatype":"json",
 
                 colModel: [
+
                     { label: '#', name: 'numeracion', key: true, width: 20,"search":false },
-                    { label: 'Coordinacion', name: 'coordinacion', width: 150 ,"search":true},
-                    { label: 'Nombres', name: 'nombres', width: 120 ,"search":true}
+                    { label: 'Nombre de la actividad', name: 'nombre_actividad', width: 150 ,"search":true},
+                    { label: 'Fecha', name: 'fecha_actividad', width: 120 ,"search":true},
+                    { label: 'Hora', name: 'hora_actividad', width: 120 ,"search":true},
+                    { label: 'Ubicación', name: 'ubicacion', width: 120 ,"search":true},
+                    { label: 'Estatus', name: 'status_actividad', width: 120 ,"search":true},
+                    { label: 'id', name: 'id_actividad_institucional', key: true, width: 1,"search":false },
                 ],
 
-                jsonReader: {repeatitems:false, root:"vacaciones"},
+                jsonReader: {repeatitems:false, root:"actividades"},
                 pager: grid_pager,
                 multiselect: true,
                 loadComplete : function() {
@@ -51,11 +56,11 @@ $(document).ready(function () {
                         addicon: 'ace-icon fa fa-plus-circle purple',
                         del: false,
                         delicon: 'ace-icon fa fa-trash-o red',
-                        search: true,
+                        search: false,
                         searchicon : 'ace-icon fa fa-search orange',
-                        refresh: true,
+                        refresh: false,
                         refreshicon : 'ace-icon fa fa-refresh green',
-                        view: true,
+                        view: false,
                         viewicon : 'ace-icon fa fa-search-plus grey',
                     },
                     {
@@ -90,9 +95,9 @@ $(document).ready(function () {
                     var replacement = 
                     {
                         'ui-icon-seek-first' : 'ace-icon fa fa-angle-double-left bigger-140',
-                        'ui-icon-seek-prev' : 'ace-icon fa fa-angle-left bigger-140',
-                        'ui-icon-seek-next' : 'ace-icon fa fa-angle-right bigger-140',
-                        'ui-icon-seek-end' : 'ace-icon fa fa-angle-double-right bigger-140'
+                        'ui-icon-seek-prev'  : 'ace-icon fa fa-angle-left bigger-140',
+                        'ui-icon-seek-next'  : 'ace-icon fa fa-angle-right bigger-140',
+                        'ui-icon-seek-end'   : 'ace-icon fa fa-angle-double-right bigger-140'
                     };
                     $('.ui-pg-table:not(.navtable) > tbody > tr > .ui-pg-button > .ui-icon').each(function(){
                         var icon = $(this);
@@ -101,6 +106,105 @@ $(document).ready(function () {
                         if($class in replacement) icon.attr('class', 'ui-icon '+replacement[$class]);
                     })
                 }
+    $(grid_selector).navButtonAdd(grid_pager,
+    {
+        buttonicon: "ace-icon fa fa-search-plus grey",
+        title: "Personal asignado",
+        caption: 'Ver personal asignado',
+        position: "last",
+        onClickButton: detalles
+    });
+
+    function detalles() {
+        var columna_check = $(grid_selector).jqGrid("getGridParam", "selarrrow")
+                                
+        if(columna_check.length>0){
+            var id_actividad_institucional = [];
+            var status_actividad = [];
+            for(var i=0,ids=columna_check.length;i<ids; i++){
+                id_actividad_institucional.push($(grid_selector).jqGrid('getCell', columna_check[i], 'id_actividad_institucional'));
+                status_actividad.push($(grid_selector).jqGrid('getCell', columna_check[i], 'status_actividad'));
+            }
+        }
+        if (id_actividad_institucional!=undefined && id_actividad_institucional!=null) {
+                 pickOpen('prod', 'id_prod',BASE_URL+'actividad_institucional/detalles/'+id_actividad_institucional,
+                60, 45, 300, 80);show('prod',500);show('id_aceptar',500);hide('id_buscar',500);
+        }else{
+            alert('Seleccione una actividad');
+        }
+    }
+    $(grid_selector).navButtonAdd(grid_pager,
+    {
+        buttonicon: "ace-icon fa fa-times-circle red",
+        title: "Finalizar actividad",
+        caption: 'Finalizar actividad',
+        position: "last",
+        onClickButton: finalizar_actividad
+    });
+
+    function finalizar_actividad() {
+        var columna_check = $(grid_selector).jqGrid("getGridParam", "selarrrow")
+                                
+        if(columna_check.length>0){
+            var id_actividad_institucional = [];
+            var status_actividad = [];
+            for(var i=0,ids=columna_check.length;i<ids; i++){
+                id_actividad_institucional.push($(grid_selector).jqGrid('getCell', columna_check[i], 'id_actividad_institucional'));
+                status_actividad.push($(grid_selector).jqGrid('getCell', columna_check[i], 'status_actividad'));
+            }
+        }
+        if (id_actividad_institucional!=undefined && id_actividad_institucional!=null) {
+            if (status_actividad=="Actividada finalizada") {
+               alert('Esta actividad ya fue finalizada');
+            }
+            else if(status_actividad=="Cancelada"){
+               alert('Esta actividad fue cancelada');
+            }
+            else{ 
+                 pickOpen('prod', 'id_prod',BASE_URL+'actividad_institucional/fin/'+id_actividad_institucional+"/TRUE",
+                60, 30, 300, 80);show('prod',500);show('id_aceptar',500);hide('id_buscar',500);                     
+            }
+
+        }else{
+            alert('Seleccione una actividad');
+        }
+    } 
+
+    $(grid_selector).navButtonAdd(grid_pager,
+    {
+        buttonicon: "ace-icon fa fa-times-circle red",
+        title: "Cancelar actividad",
+        caption: 'Cancelar actividad',
+        position: "last",
+        onClickButton: cancelar_actividad
+    });
+
+    function cancelar_actividad() {
+        var columna_check = $(grid_selector).jqGrid("getGridParam", "selarrrow")
+                                
+        if(columna_check.length>0){
+            var id_actividad_institucional = [];
+            var status_actividad = [];
+            for(var i=0,ids=columna_check.length;i<ids; i++){
+                id_actividad_institucional.push($(grid_selector).jqGrid('getCell', columna_check[i], 'id_actividad_institucional'));
+                status_actividad.push($(grid_selector).jqGrid('getCell', columna_check[i], 'status_actividad'));
+            }
+        }
+        if (id_actividad_institucional!=undefined && id_actividad_institucional!=null) {
+            if (status_actividad=="Actividada finalizada") {
+               alert('Esta actividad ya fue finalizada');
+            }
+            else if(status_actividad=="Cancelada"){
+               alert('Esta actividad ya fue cancelada');
+            }else{
+                 pickOpen('prod', 'id_prod',BASE_URL+'actividad_institucional/cancelar/'+id_actividad_institucional,
+                60, 30, 300, 80);show('prod',500);show('id_aceptar',500);hide('id_buscar',500);                     
+            }
+
+        }else{
+            alert('Seleccione una actividad');
+        }
+    }                          
 
 
         });
